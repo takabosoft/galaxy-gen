500374919afad2b8ffcdd63b522ee741a50b9e0a
takabosoft <takabosoft@gmail.com>
2025-04-13 11:12:50 +0900

    棒状銀河

--- src/shader/galaxy.ts
+++ src/shader/galaxy.ts
@@ -31,8 +31,7 @@ float fbm(vec3 p, float scaleBase, float scalePow, float depthBase, float depthP
 float armDist(vec3 pos, float r) {
     float angle = atan(pos.z, pos.x);
     float spiralStrength = -3.4;
-    float stickR = 0.0; // 棒渦巻銀河
-    float r2 = stickR == 0.0 ? r : (smax(r - stickR, 0.0, 0.4));
+    float r2 = u_stickRadius == 0.0 ? r : (smax(r - u_stickRadius, 0.0, 0.4));
     float spiralAngle = mod(angle - r2 * spiralStrength, 2.0 * PI);
     float armDist = abs(mod(spiralAngle * u_arms + PI, 2.0 * PI) - PI);
     return armDist;
