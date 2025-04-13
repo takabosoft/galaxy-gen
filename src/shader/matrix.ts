0fab3a730875d09670ae0dd21f51d457464dc163
takabosoft <takabosoft@gmail.com>
2025-04-13 09:31:01 +0900

    カメラX軸回転対応

--- src/shader/matrix.ts
+++ src/shader/matrix.ts
@@ -1,4 +1,14 @@
 export const matrix = `
+mat3 rotateX(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(
+        1.0, 0.0, 0.0,
+        0.0, c, -s,
+        0.0, s, c
+    );
+}
+
 mat3 rotateY(float theta) {
     float c = cos(theta);
     float s = sin(theta);
@@ -6,6 +16,16 @@ mat3 rotateY(float theta) {
         c, 0.0, -s,
         0.0, 1.0, 0.0,
         s, 0.0, c
+    );
+}
+
+mat3 rotateZ(float theta) {
+    float c = cos(theta);
+    float s = sin(theta);
+    return mat3(
+        c, -s, 0.0,
+        s,  c, 0.0,
+        0.0, 0.0, 1.0
     );
 }
 `;
